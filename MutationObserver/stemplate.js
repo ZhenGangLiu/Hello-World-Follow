1cd7f54da8031ac9434c69cb245a985a4fc23aa4
周鹏 <1179258516@qq.com>
2016-09-13 22:47:54 +0800

    fix bug

--- MutationObserver/stemplate.js
+++ MutationObserver/stemplate.js
@@ -1,42 +1,105 @@
-// 仿照esl语法，简单实现下模板引擎
+/**
+ * 将模板引擎转化为可用dom字符串
+ *
+ * @param {String} tpl
+ * @param {Object} data 数据对象,为键值对形式,键值为数据名  
+ * @return {String}
+ */
 function tpl2dom (tpl, data) {
+	var nbspRE = /\s{2,}/g,	
+		quotRE = /\"/g,
+		main = "";
 	function fn (d) {
-		var i, k = [], v = [];
+		var i, keys = [], vals = [];
 		for (i in d) {
-			k.push(i);
-			v.push(d[i]);
+			keys.push(i);
+			vals.push(d[i]);
 		}
-		return (new Function(k, fn.main)).apply(d, v);
+		return (new Function(keys, main)).apply(d, vals);
 	}
-	if (!fn.main) {
+	if (!main) {
 		var tpls = tpl.split("<%"),
 			len = tpls.length;
-		fn.main = `var res = "${tpls[0].replace(/\s/g, '')}";\n`;
+		main = `var res = "${tpls[0].replace(nbspRE, '').replace(quotRE, "\'")}";\n`;
 		for (var i = 1; i < len; i++) {
 			var p = tpls[i].split("%>");
-			fn.main += 0x3D === p[0].charCodeAt(0) ? 
-													"+(" + p[0].substr(1) + ")" :
-													p[0].replace(/\r\n/g, "").trim(); 		
-			fn.main += `\nres += "${p[p.length - 1]
-																.replace(/\s{2,}/g, '')
-																.replace(/\"/g, "\'")
-																.replace(/\r\n/g, '')
-																.replace(/\n/g, '\\n')
-																.replace(/\r/g, '\\n')}"`;
+			main += 0x3D === p[0].charCodeAt(0) ? 
+							`\nres += ${p[0].substr(1)}`: 
+							`\n${p[0].replace(/\r\n/g, "").trim()}`; 		
+			main += `\nres += "${p[p.length - 1]
+							.replace(nbspRE, '')
+							.replace(quotRE, "\'")
+							.replace(/[\r\n]/g, '')}"`;
 		}
-		fn.main += "\nreturn res;";
+		main += "\nreturn res;";
 	}
-	return fn;
+	return data ? fn(data) : fn();
 }
-(function () {
-	var tpl = document.getElementById("tpl"),
-		app = document.getElementById("app"),
-		cacheDiv = document.createElement("div");
-	function str2dom (str) {
-		cacheDiv.innerHTML = "";
-		cacheDiv.innerHTML = str;
-		return cacheDiv.childNodes[0];
+var cacheDiv = document.createElement("div"),
+	MutationObserver = window.MutationObserver;
+/**
+ * 将可用dom字符串转为dom节点
+ *
+ * @param {String} str
+ * @return {DOM}
+ */
+function str2dom (str) {
+	cacheDiv.innerHTML = "";
+	cacheDiv.innerHTML = str;
+	// 由此可看出,模板必须要求有一个父节点
+	return cacheDiv.childNodes[0];
+}
+/**
+ * 为对象绑定getter/setter
+ *
+ * @param {String} str
+ * @return {DOM}
+ */
+function bindObject (obj, vm) {
+	var data = {};
+	for (var k in obj) {
+		Object.defineProperty(data, k, {
+			configurable: true,
+    	enumerable: true,
+			get: function () {
+				return obj[k];
+			},
+			set: function (val) {
+				obj[k] = val;
+				// vm.render();
+			}
+		});
 	}
-	app.appendChild(str2dom(tpl2dom(tpl.innerHTML)()));
-	tpl.parentNode.removeChild(tpl);
+	return data;
+}
+function VM (obj) {
+	// 初始化
+	this.ele = obj.el;
+	this.tpl = obj.tpl;
+	this.data = bindObject(obj.data, this);
+	this.dom = this.render();
+}
+VM.prototype.render = function () {
+	var dom = str2dom(tpl2dom(this.tpl.innerHTML, this.data));
+	this.ele.appendChild(dom);	
+	app.removeChild(this.tpl);
+	return dom;
+};
+VM.prototype.change = function () {
+	// fn();
+};
+(function () { 
+	var tpl = document.getElementById("tpl"),
+		app = tpl.parentNode || document.getElementById("app");
+	var obj = {
+		msg: "Hello World!"
+	};
+	setTimeout(function () {
+		vm.data.msg = "go!go!go!";
+	}, 1000);
+	var vm = new VM({
+		el: app,
+		tpl: tpl,
+		data: obj
+	});
 })();
